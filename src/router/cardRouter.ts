3385b58b37bd0710d9372bd082b2c3f794c43ba7
dev-madilyn <lollin0@naver.com>
2022-11-20 00:39:06 +0900

    Add: Router 폴더 및 파일 추가

---
+++ src/router/cardRouter.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { cardController } from "../controller";
+
+const router: Router = Router();
+
+export default router;
---
+++ src/router/userRouter.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { userController } from "../controller";
+
+const router: Router = Router();
+
+export default router;
