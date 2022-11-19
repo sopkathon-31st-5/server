330768a17026b342d3e532a0ad541d9850752b5f
onpyeong <gsh447114@gmail.com>
2022-11-20 03:22:51 +0900

    Feat: cardRouter 설정

--- src/router/cardRouter.ts
+++ src/router/cardRouter.ts
@@ -3,4 +3,7 @@ import { cardController } from "../controller";
 
 const router: Router = Router();
 
+//* 카드 생성 - POST card/:userId
+router.post('/:userId', cardController.createCard);
+
 export default router;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import cardRouter from "./cardRouter";
 import userRouter from "./userRouter";
 
 const router: Router = Router();
 
 // router.use("/user", userRouter);
+router.use('/card', cardRouter);
 
 export default router;
