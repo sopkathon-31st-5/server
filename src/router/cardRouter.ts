e8128888adc48faab281ca50d67b82a47b70506e
dev-madilyn <lollin0@naver.com>
2022-11-20 03:20:48 +0900

    Chore: cardRouter 구현 #4

--- src/router/cardRouter.ts
+++ src/router/cardRouter.ts
@@ -3,4 +3,7 @@ import { cardController } from "../controller";
 
 const router: Router = Router();
 
+//* 명함 조회 - GET /card/:userId
+router.get("/:userId", cardController.getCard);
+
 export default router;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import cardRouter from "./cardRouter";
+
 
 const router: Router = Router();
 
-// router.use("/user", userRouter);
+router.use("/card", cardRouter);
 
 export default router;
