1490763bb3b4007067f0204bd52e482af705ad1d
yangjiyoung <0112jyoung@naver.com>
2022-11-20 00:58:35 +0900

    Feat: 유저 생성 서비스 설정

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,2 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+const createUser =async(name:String, number:String) => {
+    const user= await prisma.User.findUnique({
+        where:{
+            name:name,
+            number:number
+        }
+    });
+
+    if(user)
+        return user;
+    
+    const newUser=await prisma.User.create({
+        data: {
+            name:name,
+            number:number
+        }
+    });
+    return newUser;
+};
