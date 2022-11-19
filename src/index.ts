239b8071b821527cc64a1692c5d59aa23a39dfbc
dev-madilyn <lollin0@naver.com>
2022-11-20 03:37:29 +0900

    Feat: CORS 에러 핸들링 #7

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
+import cors from 'cors';
 
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
 
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
-
+app.use(cors());
 app.use("/", router); // use -> 모든 요청
 
 //* HTTP method - GET
