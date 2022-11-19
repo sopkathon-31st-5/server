a61fac256f937d48f9eb3499c079dd0d6ec8d93e
yangjiyoung <0112jyoung@naver.com>
2022-11-20 02:30:15 +0900

    Feat: UserDTO 생성

---
+++ src/interfaces/UserDTO.ts
@@ -0,0 +1,6 @@
+export interface UserDTO{
+    id: number,
+    name: string,
+    phoneNumber:string,
+    isNew:boolean
+}
--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { UserDTO } from "../interfaces/UserDTO";
 const prisma = new PrismaClient();
 
 const createUser =async(name:String, number:String) => {
@@ -8,8 +9,10 @@ const createUser =async(name:String, number:String) => {
         }
     });
 
-    if(user)
-        return user;
+    if(user){
+        const oldUser:UserDTO={"id":user.id, "name":user.name, "phoneNumber":user.phoneNumber,"isNew":false};
+        return oldUser;
+    }
 
     const newUser=await prisma.User.create({
         data: {
@@ -17,7 +20,8 @@ const createUser =async(name:String, number:String) => {
             phoneNumber:number
         }
     });
-    return newUser;
+    const returnUser:UserDTO={"id":newUser.id, "name":newUser.name, "phoneNumber":newUser.phoneNumber,"isNew":true};
+    return returnUser;
 };
 
 const userService= {
